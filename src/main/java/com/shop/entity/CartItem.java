eeac992948cbd8362355afc56ce7eaa4503743ac
임채성 <pan1129@naver.com>
2022-04-03 17:45:25 +0900

    스프링 장바구니 주문등록  Commit

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 @Getter @Setter
 @Table(name = "cart_item")
-public class CartItem {
+public class CartItem extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -25,6 +25,21 @@ public class CartItem {
 
     private int count;
 
+    public static CartItem createCartItem(Cart cart, Item item, int count) {
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+        return cartItem;
+    }
+
+    public void addCount(int count){
+        this.count += count;
+    }
+
+    public void updateCount(int count){
+        this.count = count;
+    }
 
 
 
