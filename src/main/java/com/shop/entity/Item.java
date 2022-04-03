001299b2e4e0215226525f19c6178c94f2945bc2
임채성 <pan1129@naver.com>
2022-04-03 13:23:05 +0900

    스프링 상품 수정 Commit

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -2,6 +2,7 @@ package com.shop.entity;
 
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -45,6 +46,14 @@ public class Item extends BaseEntity{
             inverseJoinColumns = @JoinColumn(name = "item_id")
     )
     private List<Member> member;
+
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 
 
 }
